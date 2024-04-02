0beb309b853bfc9dd58f375fe8c2fc51a46f456b
Nandhu@112 <nandhuraj112@gmail.com>
2024-04-02 12:42:24 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './App.css'
+import NavBar from "./Components/NavBar/NavBar";
+import Banner from "./Components/Banner/Banner";
+import RowPost from "./Components/RowPost/RowPost";
+import {action, originals, HorrorMovies, comedy} from './urls'
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <RowPost url={originals} title = 'Netflix Originals' />
+      <RowPost url={action} title ='Action' isSmall />
+      <RowPost url={HorrorMovies} title = 'HorrorMovies' isSmall />
+      <RowPost url={comedy} title = 'ComedyMovies' isSmall />
+    </div>
+  );
+}
+
+export default App;
